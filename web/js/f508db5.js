6d5b5e284ba93c4aee9e7fdb384d27f0bb742520
Leo Chen <yhnehc@gmail.com>
2017-05-02 09:19:21 -0700

    remove

--- web/js/f508db5.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	// Bind on change of location
-	$('body').unbind('change.scannedStock'); // required 
-	$('body').on('change.scannedStock', '.incomingScannedProductsPage select, .incomingScannedProductsPage input', function() {
-		$('#scanned_form_wrap .loading').show();
-		$('#incoming-product-page-form').submit();
-	});
-});
