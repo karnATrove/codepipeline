acab201de139cadefa7f8f1f96600072a58be676
yhnehc <yhnehc@gmail.com>
2017-06-27 10:24:32 -0700

    update

---
+++ src/RoveSiteRestApiBundle/Exception/DeserializeException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rove
+ * Date: 2017-06-27
+ * Time: 10:21 AM
+ */
+
+namespace RoveSiteRestApiBundle\Exception;
+
+
+class DeserializeException extends \Exception
+{
+
+}
--- src/RoveSiteRestApiBundle/Utils/SerializeHelper.php
+++ src/RoveSiteRestApiBundle/Utils/SerializeHelper.php
@@ -11,6 +11,7 @@ namespace RoveSiteRestApiBundle\Utils;
 
 use Rove\CanonicalDto\Response\ResponseErrorDto;
 use Rove\CanonicalDto\Response\ResponseErrorItemDto;
+use RoveSiteRestApiBundle\Exception\DeserializeException;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
@@ -23,18 +24,23 @@ class SerializeHelper
 	 * @param string $responseErrorDto
 	 *
 	 * @return ResponseErrorDto
+	 * @throws DeserializeException
 	 */
 	public static function deserializeResponseErrorDto($responseErrorDto)
 	{
 		$serializer = new Serializer(
 			[new ObjectNormalizer(null, new CamelCaseToSnakeCaseNameConverter()),  new ArrayDenormalizer()],
 			[new JsonEncoder()]);
-		/** @var ResponseErrorDto $responseErrorDto */
-		$responseErrorDto = $serializer->deserialize($responseErrorDto, ResponseErrorDto::class, 'json');
-		/** @var ResponseErrorItemDto[] $errorItems */
-		$errorItems = $serializer->denormalize($responseErrorDto->getErrorItems(),
-			ResponseErrorItemDto::class . '[]');
-		$responseErrorDto->setErrorItems($errorItems);
-		return $responseErrorDto;
+		try{
+			/** @var ResponseErrorDto $responseErrorDto */
+			$responseErrorDto = $serializer->deserialize($responseErrorDto, ResponseErrorDto::class, 'json');
+			/** @var ResponseErrorItemDto[] $errorItems */
+			$errorItems = $serializer->denormalize($responseErrorDto->getErrorItems(),
+				ResponseErrorItemDto::class . '[]');
+			$responseErrorDto->setErrorItems($errorItems);
+			return $responseErrorDto;
+		}catch (\Exception $exception){
+			throw new DeserializeException("Failed to deserialize response error DTO. Response is not in a proper format.");
+		}
 	}
 }
