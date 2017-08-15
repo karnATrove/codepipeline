56fbac8a0e0b6adec00b0f178c77ee01f84d9950
Lee Chen <leechen@roveconcepts.com>
2017-08-15 15:21:35 -0700

    update

---
+++ src/WarehouseBundle/Model/Booking/BookingProductSearchModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lee
+ * Date: 2017-08-15
+ * Time: 3:17 PM
+ */
+
+namespace WarehouseBundle\Model\Booking;
+
+
+class BookingProductSearchModel
+{
+
+}
