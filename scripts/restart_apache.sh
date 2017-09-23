5efac138f10b1e72c8bc82d0fd432e14a7a3ad9b
karnATrove <karn.s.rove@gmail.com>
2017-09-23 02:35:10 -0700

    Update restart_apache.sh

--- scripts/restart_apache.sh
+++ scripts/restart_apache.sh
@@ -3,4 +3,4 @@
 # exec 3>&1 4>&2
 # trap 'exec 2>&4 1>&3' 0 1 2 3
 # exec 1>/var/www/vhosts/logs/out.log 2>&1
-# service httpd restart > /var/www/vhosts/logs/restartapache.out 2>&1
+service httpd restart > /var/www/vhosts/logs/restartapache.out 2>&1
