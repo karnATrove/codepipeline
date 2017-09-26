fe2bb34e87040dc6e70f3f2622ba5aaa53fd3f8e
karnATrove <karn.s.rove@gmail.com>
2017-09-25 19:28:31 -0700

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -5,4 +5,8 @@
 # exec 1>/var/www/vhosts/logs/out.log 2>&1
 chmod -R 777 /var/www/vhosts/rlogistic.roveconcepts.me/public_html/var/cache
 chmod -R 777 /var/www/vhosts/rlogistic.roveconcepts.me/public_html/var/logs
+#cd ~
+#curl -sS https://getcomposer.org/installer | php
+#mv composer.phar /usr/local/bin/composer
+#ln -s /usr/local/bin/composer /usr/bin/composer
 service httpd restart > /var/www/vhosts/logs/restartapache.out 2>&1
