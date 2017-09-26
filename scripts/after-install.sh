6e3e41c2a68ad6debf5b9b466e430aa1bed6db05
karnATrove <karn.s.rove@gmail.com>
2017-09-25 21:15:59 -0700

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -12,7 +12,7 @@ chown -R ec2-user:apache /etc/httpd/conf
 chown -R ec2-user:apache /etc/httpd/conf.d
 cd ~
 curl -sS https://getcomposer.org/installer | php
-#mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
 #ln -s /usr/local/bin/composer /usr/bin/composer
 #composer upadte
 #php bin/console cache:clear --env=prod
