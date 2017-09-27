c78956be1173b1975e08e22139360d50f15da448
karnATrove <karn.s.rove@gmail.com>
2017-09-26 18:51:04 -0700

    Update app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -30,24 +30,63 @@ function ip_in_range( $ip, $range ) {
 $ip = $_SERVER['REMOTE_ADDR'];
 echo ($ip.'<br>');
 $in_range = false;
-$cidr_list = array("10.1.42.0/24","10.1.44.0/24","20.1.42.0/24","20.1.44.0/24","30.1.42.0/24","30.1.44.0/24","10.0.42.0/24","10.0.44.0/24","20.0.42.0/24","20.0.44.0/24","30.0.42.0/24","30.0.44.0/24");
+$cidr_list = array( "10.1.42.0/24",
+                    "10.1.44.0/24",
+                    "20.1.42.0/24",
+                    "20.1.44.0/24",
+                    "30.1.42.0/24",
+                    "30.1.44.0/24",
+                    "10.0.42.0/24",
+                    "10.0.44.0/24",
+                    "20.0.42.0/24",
+                    "20.0.44.0/24",
+                    "30.0.42.0/24",
+                    "30.0.44.0/24");
 foreach($cidr_list as $cidr) {
   $in_range = $in_range || (ip_in_range($ip,$cidr));
   if($in_range) { break; }
 }
 echo ($in_range.'<br>');
 
+if($in_range != 1) {
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
+  if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '23.16.26.235', '24.86.40.31', '162.219.176.101', '158.85.122.228', '206.116.49.142', '54.218.217.232','24.84.45.15','70.197.80.176','70.197.68.46','70.197.70.147','104.152.233.75','70.211.145.160','104.174.121.222','70.197.69.17','173.183.27.49','70.79.50.163','108.47.15.14','70.211.134.189','199.116.75.130','12.35.79.25','::1','207.216.41.143','207.216.41.188','207.216.41.189')) || php_sapi_name() === 'cli-server')
-    || ($in_range != 1))
-{
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array(
+        '127.0.0.1',
+        '23.16.26.235',
+        '24.86.40.31',
+        '162.219.176.101',
+        '158.85.122.228',
+        '206.116.49.142',
+        '54.218.217.232',
+        '24.84.45.15',
+        '70.197.80.176',
+        '70.197.68.46',
+        '70.197.70.147',
+        '104.152.233.75',
+        '70.211.145.160',
+        '104.174.121.222',
+        '70.197.69.17',
+        '173.183.27.49',
+        '70.79.50.163',
+        '108.47.15.14',
+        '70.211.134.189',
+        '199.116.75.130',
+        '12.35.79.25',
+        '::1',
+        '207.216.41.143',
+        '207.216.41.188',
+        '207.216.41.189'
+      )) || php_sapi_name() === 'cli-server')
+  ) {
     header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information. IP: '.$_SERVER['REMOTE_ADDR']);
-} else {
-  echo (($_SERVER['REMOTE_ADDR'].' is eligible'));
+    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information. IP: ' . $_SERVER['REMOTE_ADDR']);
+  }
+}else {
+  echo(($_SERVER['REMOTE_ADDR'] . ' is eligible'));
 }
 
 /** @var \Composer\Autoload\ClassLoader $loader */
