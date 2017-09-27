44f907e2f1eb35330a6be76e4b9029dc752bfd84
karnATrove <karn.s.rove@gmail.com>
2017-09-26 18:08:24 -0700

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -19,10 +19,10 @@ ln -s /usr/local/bin/composer /usr/bin/composer
 #php bin/console assetic:dump
 #yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https -y
 mkdir /CloudWatch
-#cd /CloudWatch
-#curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
-#unzip CloudWatchMonitoringScripts-1.2.1.zip
-#rm -f CloudWatchMonitoringScripts-1.2.1.zip
+cd /CloudWatch
+curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
+unzip CloudWatchMonitoringScripts-1.2.1.zip
+rm -f CloudWatchMonitoringScripts-1.2.1.zip
 #cd aws-scripts-mon
 #./mon-put-instance-data.pl --mem-util --mem-used-incl-cache-buff --mem-used --mem-avail
 #./mon-put-instance-data.pl --mem-util --mem-used --mem-avail --auto-scaling=only
@@ -31,4 +31,10 @@ mkdir /CloudWatch
 #chmod -R 777 /var/www/
 #chmod -R 777 /var/www/vhosts/dx3pl.roveconcepts.me/public_html/var/cache
 #chmod -R 777 /var/www/vhosts/dx3pl.roveconcepts.me/public_html/var/logs
+chown -R ec2-user:apache /var/www
+chmod -R 777 /var/www/
+chmod -R 777 /var/www/vhosts/dx3pl.roveconcepts.me/public_html/var/cache
+chmod -R 777 /var/www/vhosts/dx3pl.roveconcepts.me/public_html/var/logs
+chmod -R 777 /var/www/vhosts/rlogistic.roveconcepts.me/public_html/var/cache
+chmod -R 777 /var/www/vhosts/rlogistic.roveconcepts.me/public_html/var/logs
 service httpd restart > /var/www/vhosts/logs/restartapache.out 2>&1
