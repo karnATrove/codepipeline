c9ce90594b259a2ad65ca998178623da287cab23
karnATrove <karn.s.rove@gmail.com>
2017-09-28 16:59:09 -0700

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -15,6 +15,14 @@ chown -R ec2-user:apache /var/www
 chown -R ec2-user:apache /etc/httpd/conf
 chown -R ec2-user:apache /etc/httpd/conf.d
 
+###############################################################################
+# Change parameter in php.ini, this must be done before clearing composer cache
+###############################################################################
+sed -i 's/memory_limit = 128M/memory_limit = 2048M/g' /etc/php.ini
+cat /etc/php.ini | grep 'memory_limit' >> /var/www/vhosts/logs/after_install_script.out 2>&1
+service httpd restart >> /var/www/vhosts/logs/after_install_script.out 2>&1
+#sudo sed -i 's/128M/2048M/g' /etc/php.ini
+
 #######################################################################
 # Install composer
 # Execute composer install if parameters.yml file is not yet created
@@ -29,26 +37,30 @@ ln -s /usr/local/bin/composer /usr/bin/composer
 cd /var/www/vhosts/rlogistic.roveconcepts.me/public_html/app/config
 if [ -e parameters.yml ]
 then
-    echo "ok parameters.yml or rlogistic exist"
+    echo "parameters.yml of rlogistic already existed" >> /var/www/vhosts/logs/after_install_script.out 2>&1
 else
+    echo "no parameters.yml of rlogistic exist" >> /var/www/vhosts/logs/after_install_script.out 2>&1
     cd /var/www/vhosts/rlogistic.roveconcepts.me/public_html/web
     composer install --no-interaction
+    cd /var/www/vhosts/rlogistic.roveconcepts.me/public_html
+    echo "Clear rlogistic cache" >> /var/www/vhosts/logs/after_install_script.out 2>&1
+    php -d memory_limit=2048M bin/console cache:clear --env=prod >> /var/www/vhosts/logs/after_install_script.out 2>&1
+    php -d memory_limit=2048M bin/console assetic:dump >> /var/www/vhosts/logs/after_install_script.out 2>&1
 fi
+
 cd /var/www/vhosts/dx3pl.roveconcepts.me/public_html/app/config
 if [ -e parameters.yml ]
 then
-    echo "ok parameters.yml or dx3pl exist"
+    echo "parameters.yml of dx3pl already existed" >> /var/www/vhosts/logs/after_install_script.out 2>&1
 else
+    echo "no parameters.yml of dx3pl exist" >> /var/www/vhosts/logs/after_install_script.out 2>&1
     cd /var/www/vhosts/dx3pl.roveconcepts.me/public_html/web
     composer install --no-interaction
+    cd /var/www/vhosts/dx3pl.roveconcepts.me/public_html
+    echo "Clear dx3pl cache" >> /var/www/vhosts/logs/after_install_script.out 2>&1
+    php -d memory_limit=2048M bin/console cache:clear --env=prod >> /var/www/vhosts/logs/after_install_script.out 2>&1
+    php -d memory_limit=2048M bin/console assetic:dump >> /var/www/vhosts/logs/after_install_script.out 2>&1
 fi
-#composer update
-#cd /var/www/vhosts/rlogistic.roveconcepts.me/public_html
-#php -d memory_limit=2048M bin/console cache:clear --env=prod
-#php -d memory_limit=2048M bin/console assetic:dump
-#cd /var/www/vhosts/dx3pl.roveconcepts.me/public_html
-#php -d memory_limit=2048M bin/console cache:clear --env=prod
-#php -d memory_limit=2048M bin/console assetic:dump
 #cat /var/www/vhosts/rlogistic.roveconcepts.me/public_html/app/config/parameters.yml | grep database_host
 #echo 'export database_host=rlogistic-cluster.cluster-cepwew4s61wr.us-west-2.rds.amazonaws.com' > /var/www/vhosts/config/configuration.sh
 
